d97fa18fc01d74b09c9185f78c91676929ba1a76
Nate <ief015@gmail.com>
2022-10-22 01:18:02 -0400

    block nav buttons with ripple

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,11 +1,12 @@
 <template>
   <v-sheet class="py-3" elevation="2">
     <v-row justify="center">
-      <v-col cols="auto" v-for="nav in navs">
+      <v-col cols="auto pa-0" v-for="nav in navs">
         <router-link
-          class="text-decoration-none"
+          class="nav-link text-decoration-none pa-3 d-block"
           exact-active-class="font-weight-bold"
           :to="nav.path"
+          v-ripple
         >
           {{ nav.text }}
         </router-link>
@@ -30,3 +31,10 @@ const navs: NavRoute[] = [
   },
 ];
 </script>
+
+<style scoped>
+.nav-link:hover {
+  background-color: #333 !important;
+  transition: 0.2s;
+}
+</style>
