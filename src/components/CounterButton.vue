9e04fd996134b69dcdfbf45ec2ff0d3bb8d42360
Nate <ief015@gmail.com>
2022-10-22 02:26:45 -0400

    example counter store

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn @click="onClick">{{counter.count}}</v-btn>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/CounterStore';
+
+const counter = useCounterStore();
+
+const onClick = () => {
+  counter.increment();
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container class="text-center">
-    <p>
+    <div class="my-2">
       Nice about page! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sit eius fuga quos atque voluptates, nesciunt similique nisi esse accusamus neque tenetur, officia ad sequi incidunt mollitia ipsam optio nostrum?
-    </p>
+    </div>
+    <div class="my-2">
+      <counter-button />
+    </div>
   </v-container>
 </template>
+
+<script setup lang="ts">
+import CounterButton from '@/components/CounterButton.vue';
+</script>
