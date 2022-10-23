21fd223121136a53f426665938ad268b93034de2
Nate <ief015@gmail.com>
2022-10-22 20:29:03 -0400

    dynamic footer reimpl in config

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,15 +1,24 @@
 <template>
-  <v-sheet class="pa-3">
+  <v-sheet class="pa-3" v-if="footer">
     <v-row class="justify-end text-grey text-caption">
-      <v-col cols="auto">
-        <a href="https://github.com/ief015/vuetify-vite" target="_blank">
-          GitHub
+      <v-col cols="auto" v-for="link in footer.links">
+        <a :href="link.href" target="_blank">
+          {{ link.text }}
         </a>
       </v-col>
-      <v-divider vertical inset />
+      <v-divider vertical inset v-if="showDivider" />
       <v-col cols="auto" class="font-weight-bold">
-        Vite ‒ Vue 3 ‒ Vuetify 3 ‒ Vue Router ‒ TypeScript ‒ Pinia
+        {{ footer.text }}
       </v-col>
     </v-row>
   </v-sheet>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import config from '@/config.json';
+
+const footer = config['footer'];
+const showDivider = computed(() => footer?.text && footer?.links);
+
+</script>
