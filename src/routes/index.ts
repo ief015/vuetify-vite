acb0ca3b27353b9264ef4f122f7eeaae9921685b
Nate <ief015@gmail.com>
2022-10-22 20:16:36 -0400

    Remove unused route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,7 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', name: 'home', component: () => import('@/views/Home.vue'), meta: { title: "Home" } },
-  { path: '/about', name: 'about', component: () => import('@/views/About.vue'), meta: { title: "About" },
-    children: [
-      { path: 'child', name: 'child', component: () => import('@/views/About.vue'), meta: { title: "Child" } },
-    ],
-  },
+  { path: '/about', name: 'about', component: () => import('@/views/About.vue'), meta: { title: "About" } },
 
   // Catch-all 404 route. Keep this last.
   { path: '/:pathMatch(.*)*', component: () => import('@/views/404.vue') },
